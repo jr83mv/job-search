d0e64040a1d474b9c044a44f9168d6d288799146
sandeepr-19 <sandeep.rqs@gmail.com>
2023-07-18 15:40:44 +0530

    refactor(cors):modify cors

--- frontend/src/screens/mainPage.tsx
+++ frontend/src/screens/mainPage.tsx
@@ -23,14 +23,14 @@ function MainPage(props: any) {
         method: 'POST',
         body: JSON.stringify(reqBody),
         mode: "cors",
-        headers: {
-          // 'origin':'https://job-search-frontend-mz19m6p6m-jr83mv.vercel.app',
-          'Content-type': 'application/json; charset=UTF-8',
-          'credentials': 'include',
-          'withCredentials': 'true',
-          'host':'vercel.app',
-          'accept': 'application/json',
-        },
+        // headers: {
+        //   // 'origin':'https://job-search-frontend-mz19m6p6m-jr83mv.vercel.app',
+        //   'Content-type': 'application/json; charset=UTF-8',
+        //   'credentials': 'include',
+        //   'withCredentials': 'true',
+        //   'host':'vercel.app',
+        //   'accept': 'application/json',
+        // },
       })
         .then((response) => response.json())
         .then((data) => {
